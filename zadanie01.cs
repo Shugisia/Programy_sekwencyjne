18bfc46b74375fc1992387b0b4dbbfae7d2a0286
Wanessa Witkowska <128226892+Shugisia@users.noreply.github.com>
2023-06-20 19:34:24 +0200

    Add files via upload

---
+++ zadanie01.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj wartość n:");
+        int n = int.Parse(Console.ReadLine());
+
+        int policz = 0;
+        int numer = 2;
+
+        while (policz < n)
+        {
+            if (SprawdzCzyPierwsza(numer))
+            {
+                Console.WriteLine(numer);
+                policz++;
+            }
+            numer++;
+        }
+    }
+
+    static bool SprawdzCzyPierwsza(int numer)
+    {
+        if (numer < 2)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(numer); i++)
+        {
+            if (numer % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ zadanie03.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj wysokość trójkąta:");
+        int wysokosc = Convert.ToInt32(Console.ReadLine());
+
+        RysujTrojkat(wysokosc);
+
+        Console.ReadLine();
+    }
+
+    static void RysujTrojkat(int wysokosc)
+    {
+        int szerokosc = 2 * wysokosc - 1;
+        int spacje = (szerokosc - 1) / 2;
+
+        for (int i = 0; i < wysokosc; i++)
+        {
+            for (int j = 0; j < spacje; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 0; j < 2 * i + 1; j++)
+            {
+                Console.Write("+");
+            }
+
+            Console.WriteLine();
+            spacje--;
+        }
+    }
+}
